a5edadb7dad683451d7d56a02647e067c4c420a1
Alexander Dunkel <alexanderdunkel@hotmail.com>
2019-03-05 14:58:19 -0600

    Add job model

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Job(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	jobid = models.CharField(max_length=50)
+	color = models.CharField(max_length=20, blank=True)
+	value = models.IntegerField(null=True, blank=True)
+	image = models.ImageField(blank=True)
+	para1name = models.CharField(max_length=20, blank=True)
+	para2name = models.CharField(max_length=20, blank=True)
+	para1value = models.IntegerField(null=True, blank=True)
+	para2value = models.IntegerField(null=True, blank=True)
+
+	def __str__(self):
+		return '%s' % self.jobid
