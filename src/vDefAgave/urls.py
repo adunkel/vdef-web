3f2367283720865a3243d0ffe73a6d39fa913fe7
Alexander Dunkel <alexanderdunkel@hotmail.com>
2019-03-28 08:51:54 -0500

    Update media settings

--- src/vDefAgave/urls.py
+++ src/vDefAgave/urls.py
@@ -1,22 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
 	path('', views.home, name='vDefAgave-home'),
 	path('apps/', views.apps, name='vDefAgave-apps'),
 	path('systems/', views.systems, name='vDefAgave-systems'),
-	# path('jobsetup/', views.jobsetup, name='vDefAgave-jobsetup'),
-	# path('jobsubmit/', views.jobsubmit, name='vDefAgave-jobsubmit'),
-	# path('joboutput/<str:jobId>/', views.joboutput, name='vDefAgave-joboutput'),
-	# path('jobsearch/', views.jobsearch, name='vDefAgave-jobsearch'),
-	# path('chart/<str:jobName>/', views.chart, name='vDefAgave-chart'),
-	# path('api/data/<str:jobName>/', views.getData, name='vDef-getData'),
-	# path('dataview/', views.dataView, name='vDefAgave-dataview'),
 ]
-
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
